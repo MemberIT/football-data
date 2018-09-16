2cc1d09491a543b0235755ccad27d9d89feada0d
Mikhail Konyakhin <m.konyahin@gmail.com>
2018-09-16 14:08:59 +0300

    Add minimal project.

---
+++ README.md
@@ -0,0 +1,14 @@
+England Premier League Table
+============================
+
+* Install requirements and run app
+```
+apt-get install python3-venv python3-dev
+python3 -m venv venv
+pip3 install -r requirements.txt
+export SECRET_KEY=__YUOR__API__KEY__
+flask run
+```
+
+* Open [url](http://127.0.0.1:5000/)
+
---
+++ app/standings.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import json
+import requests
+import os
+
+from flask import render_template
+from app import app
+
+
+@app.route('/')
+def standings():
+    SECRET_KEY = os.environ.get("SECRET_KEY", default=None)
+    if not SECRET_KEY:
+        raise ValueError("No secret key set for Flask application")
+    id_competitions = '2021'
+    api = 'https://api.football-data.org'
+    uri = '/v2/competitions/' + id_competitions + '/standings'
+    headers = { 'X-Auth-Token': SECRET_KEY }
+    try:
+        response = requests.get(api + uri, headers = headers)
+        response.raise_for_status()
+        content = response.json()
+        table = content['standings'][0]['table']
+        return render_template("succes.html", data = table)
+    except requests.exceptions.Timeout:
+        return render_template("timeout.html", api = api)
+    except requests.exceptions.ConnectionError:
+        return render_template("connection_error.html")
+    except requests.exceptions.HTTPError as err:
+        content = json.loads(err.response.content.decode())
+        return render_template("error.html", content = content)
+    except:
+        print("Unexpected error:", sys.exc_info()[0])
+        raise
+
